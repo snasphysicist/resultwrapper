a5b2d747155f9c4f719c35459b5846dfda5dea6f
snasphysicist <ih8coldplay@hotmail.com>
2020-01-21 20:30:12 +0800

    Methods indicate operation is failure or success

--- src/main/java/pw/snas/resultwrapper/Outcome.java
+++ src/main/java/pw/snas/resultwrapper/Outcome.java
@@ -6,11 +6,27 @@ public class Outcome {
     private static final String DEFAULT_MESSAGE = "Success has not been confirmed";
 
     private boolean success;
-    private String failureMessage;
+    private String failureReason;
 
     public Outcome() {
         success = false;
-        failureMessage = DEFAULT_MESSAGE;
+        failureReason = DEFAULT_MESSAGE;
+    }
+
+    public void succeed() {
+        success = true;
+        failureReason = "";
+    }
+
+    public void fail(String message) {
+        // If the reason has not yet been updated
+        if (DEFAULT_MESSAGE.equals(failureReason)) {
+            // Overwrite it completely
+            failureReason = message;
+        } else {
+            // Otherwise append new message
+            failureReason = failureReason + " " + message;
+        }
     }
 
 }
