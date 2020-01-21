e34be65918cb64571cc5709977f2c0578fff8ed4
snasphysicist <ih8coldplay@hotmail.com>
2020-01-21 20:23:31 +0800

    Methods to indicate failure/success

--- src/main/java/pw/snas/resultwrapper/Result.java
+++ src/main/java/pw/snas/resultwrapper/Result.java
@@ -15,4 +15,20 @@ public class Result<T> {
         result = null;
     }
 
+    public void succeed(T result) {
+        this.result = result;
+        success = true;
+        failureReason = "";
+    }
+
+    public void fail(String message) {
+        // If first time adding a reason for failure
+        if (DEFAULT_REASON.equals(failureReason)) {
+            // Overwrite default
+            failureReason = message;
+        } else {
+            failureReason = failureReason + " " + message;
+        }
+    }
+
 }
